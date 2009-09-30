4ab095fb37c3c39a90ba3604092b30fe83beed64
Patrick Hunt <phunt1@gmail.com>
2009-09-29 23:01:34 -0700

    initial version - almost works, async is segfaulting python

--- zk-smoketest.py
+++ zk-smoketest.py
@@ -16,7 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import zookeeper, time, threading
+import zookeeper, time, datetime, threading
 from optparse import OptionParser
 
 usage = "usage: %prog [options]"
@@ -24,13 +24,15 @@ parser = OptionParser(usage=usage)
 parser.add_option("", "--servers", dest="servers",
                   default="localhost:2181", help="comma separated list of host:port")
 
+TIMEOUT = 10.0
+
 (options, args) = parser.parse_args()
 
 zookeeper.set_log_stream(open("cli_log.txt","w"))
 
 ZOO_OPEN_ACL_UNSAFE = {"perms":0x1f, "scheme":"world", "id" :"anyone"}
 
-class ZKClient:
+class ZKClient(object):
     def __init__(self, servers, timeout):
         self.connected = False
         self.conn_cv = threading.Condition( )
@@ -76,32 +78,93 @@ class ZKClient:
     def get_children(self, path, watcher=None):
         return zookeeper.get_children(self.handle, path, watcher)
 
+    def async(self, path = "/"):
+        print("ASYNC")
+        zookeeper.async(self.handle, path)
+        #time.sleep(1)
+
+class SmokeError(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
 
 if __name__ == '__main__':
+    servers = options.servers.split(",")
+
     # create on first server
-    zk = ZKClient(options.servers, 30.0)
+    zk = ZKClient(servers[0], TIMEOUT)
 
-    for i in xrange(0,100):
-        zk.create("/f1", "f1data", zookeeper.EPHEMERAL)
+    rootpath = "/zk-smoketest"
 
-        ## children
+    if zk.exists(rootpath):
+        print("Node %s already exists, attempting reuse" % (rootpath))
+    else:
+        zk.create(rootpath,
+                  "smoketest root, delete after test done, created %s" %
+                  (datetime.datetime.now().ctime()))
 
-    # set watches on all servers, one client per server
-    for server in options.servers.split(","):
-        zk = ZKClient(options.servers, 30.0)
+    # make sure previous cleaned up properly
+    children = zk.get_children(rootpath)
+    if len(children) > 0:
+        raise SmokeError("there should be not children in %s"
+                         "root data is: %s, count is %d"
+                         % (rootpath, zk.get(rootpath), len(children)))
 
-        for i in xrange(0,100):
-            print("Get:" + str(zk.get("/f1", watcher)[0]))
+    zk.close()
 
-        ## children
+    def child_path(i):
+        return "%s/session_%d" % (rootpath, i)
+
+    sessions = []
+    # create znodes, one znode per client, one client per server
+    for i, server in enumerate(servers):
+        sessions.append(ZKClient(server, TIMEOUT))
+
+        ## create child znode
+        sessions[i].create(child_path(i), "", zookeeper.EPHEMERAL)
+
+    class Watcher(object):
+        def __init__(self):
+            self.count = 0
+
+        def __call__(self, handle, typ, state, path):
+            self.count += 1
+
+    watchers = []
+    for i, server in enumerate(servers):
+        # ensure this server is up to date with leader
+        sessions[i].async()
+
+        children = sessions[i].get_children(rootpath)
+        if len(children) != len(sessions):
+            raise SmokeError("server %s wrong number of children: %d" %
+                             (server, len(children)))
+
+        watchers.append(Watcher())
+        sessions[i].get(child_path(i), watchers[i])
 
     # trigger watches
-    for i in xrange(0,100):
-        zk.set("/f1", "foo")
+    for i, server in enumerate(servers):
+        sessions[i].delete(child_path(i))
 
     # check all watches fired
+    for i, watcher in enumerate(watchers):
+        # ensure this server is up to date with leader
+        sessions[i].async()
+        if watcher.count != len(sessions):
+            raise SmokeError("server %s wrong number of watches: %d" %
+                             (server, watcher.count))
 
-    ## nodes and children
+    # close sessions
+    for i, server in enumerate(servers):
+        # ephemerals will be deleted automatically
+        sessions[i].close()
 
-    # close everything
+    # cleanup root node
+    zk = ZKClient(servers[-1], TIMEOUT)
+    # ensure this server is up to date with leader (ephems)
+    sessions[i].async()
+    zk.delete(rootpath)
+
     zk.close()
